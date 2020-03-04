16df252e8b594e02c6fcce4b4793c08d769e1ef9
R. Christian McDonald <rcmcdonald91@gmail.com>
2020-03-04 17:06:25 -0500

    Update schools.sql

--- sql/schools.sql
+++ sql/schools.sql
@@ -16,12 +16,22 @@ SELECT
 	
 	cs.Zip as 'School_zip',
 	
-	cs.Phone as 'School_phone'
+	cs.Phone as 'School_phone',
+	
+	CONCAT(st.FirstName, ' ', st.LastName) as 'Principal',
+	
+	st.Email as 'Principal_email'
 	
 FROM
-
 	dbo.ConfigSchool cs
+
+JOIN dbo.StaffSchools ss
+	ON ss.SchoolCode = cs.SchoolCode
+	
+JOIN dbo.Staff st
+	ON st.StaffID = ss.StaffID
 	
 WHERE
-
 	cs.Active = 1
+	AND st.Active = 1
+	AND st.StaffID IN (SELECT MIN(st.StaffID) FROM dbo.Staff st WHERE st.Occupation = 'Principal' GROUP BY st.StaffID)
