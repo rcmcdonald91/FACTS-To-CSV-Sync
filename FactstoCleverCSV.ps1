611f915068ea7ddfc1a8b6c6a68c8690e22e2e96
R. Christian McDonald <rcmcdonald91@gmail.com>
2020-05-19 22:46:05 -0400

    Update FactstoCleverCSV.ps1
    This revision moves configuration data to a JSON file.

--- FactstoCleverCSV.ps1
+++ FactstoCleverCSV.ps1
@@ -1,64 +1,107 @@
 param (
 
-    [string]$sqlPath = "$PSScriptRoot\sql",
-
-    [string]$outputPath = "$PSScriptRoot\output" 
+    [string]$ConfigFile = "$PSScriptRoot\config.json"
 
 )
 
-Write-Host "[$(Get-Date)] Scripted Started..." -ForegroundColor Green
+# System Settings 
+$textEncoding = [System.Text.Encoding]::UTF8 
 
-#SETs FACTS ODBC CREDENTIALS
-$factsLoginName = "<REPLACE ME>"
-$factsPassword = "<REPLACE ME>"
-$factsDatabaseName = "<REPLACE ME>"
-$factsServer = "<REPLACE ME>"
-$factsPort = "<REPLACE ME>"
+$config = Get-Content -Path $ConfigFile | ConvertFrom-Json
+
+if($config.Logging.Enabled) {
+
+    Start-Transcript -OutputDirectory $config.Logging.LogDir
+    
+}
+
+Write-Host "[$(Get-Date)] Script Started..." -ForegroundColor Green
+
+Write-Host "[$(Get-Date)] Host: $env:computername" -ForegroundColor Green
 
 ##################################
 
-Get-ChildItem -Path $sqlPath -Filter *.sql | ForEach-Object {
+# DELETE OLD CSV FILES
+Get-ChildItem -Path $config.General.OutputDir -Recurse -Force | Where-Object { -not ($_.PSIsContainer) } | Remove-Item -Force
 
-    #CREATE ODBC CONNECTION OBJECT
+# ITERATE THROUGH EACH SQL QUERY AND EXECUTE THEM
+Get-ChildItem -Path $config.General.SqlDir -Filter *.sql | ForEach-Object {
+
+    Write-Host "[$(Get-Date)] [SQL] [CONNECTION] $($_.FullName)" -ForegroundColor Green
+
+    # CREATE NEW ODBC CONNECTION OBJECT
     $conn = New-Object System.Data.Odbc.OdbcConnection
 
-    #CREATE ODBC CONNNECTION STRING
-    $conn.ConnectionString = "Driver={SQL Server};Server=$factsServer,$factsPort;Database=$factsDatabaseName;Uid=$factsLoginName;Pwd=$factsPassword"
-
+    # CREATE ODBC CONNNECTION STRING
+    $conn.ConnectionString = "Driver={{SQL Server}};Server={0},{1};Database={2};Uid={3};Pwd={4}" -f $config.ODBC.ServerAddress,$config.ODBC.ServerPort,$config.ODBC.DatabaseName,$config.ODBC.LoginName,$config.ODBC.LoginPassword
+  
     Try {
 
-        #OPEN ODBC CONNECTION
+        # TRY TO CONNECT TO THE ODBC CONNECTION
         $conn.open()
 
     } Catch {
 
-        Write-Error "[$(Get-Date)] ERROR: ODBC CONNECTION ERROR" -ErrorAction Stop 
+        Write-Error "[$(Get-Date)] [SQL] [ERROR] ODBC CONNECTION ERROR" -ErrorAction Stop 
 
     }
 
-    #READ SQL QUERY (.sql same name as script)
+    # READ SQL QUERY (.sql same name as script)
     $query = Get-Content $_.FullName
 
-    Write-Host "[$(Get-Date)] Executing: $($_.FullName)" -ForegroundColor Green
+    Write-Host "[$(Get-Date)] [SQL] [QUERY] $($_.FullName)" -ForegroundColor Green
 
-    #EXECUTE SQL QUERY
+    # EXECUTE SQL QUERY
     $cmd = New-object System.Data.Odbc.OdbcCommand($query, $conn)
 
-    #CREATE ODBC DATASET
+    # CREATE ODBC DATASET
     $ds = New-Object system.Data.DataSet
 
-    #FILL ODBC DATASET WITH DATA
+    # FILL ODBC DATASET WITH DATA
     (New-Object system.Data.odbc.odbcDataAdapter($cmd)).fill($ds) | out-null
 
-    #CLOSE AND CLEANUP
+    # CLOSE AND CLEANUP
     $conn.close()
 
-    $csvPath = "$outputPath\$($_.BaseName).csv"
+    $csvFile = $config.General.OutputDir, "$($_.BaseName).csv" -join "\"
 
-    Write-Host "[$(Get-Date)] Writing: $csvPath" -ForegroundColor Green
+    Write-Host "[$(Get-Date)] [CSV] [WRITE] $csvFile" -ForegroundColor Green
 
-    $ds.Tables[0] | Export-Csv -Path "$csvPath" -NoTypeInformation
+    # WRITE CSV TO DISK
+    $ds.Tables[0] | Export-Csv -Path $csvFile -NoTypeInformation
 
 }
 
-Write-Host "[$(Get-Date)] Scripted completed..." -ForegroundColor Green
+Write-Host "[$(Get-Date)] Script Completed..." -ForegroundColor Green
+
+if($config.Logging.Enabled) {
+
+    Stop-Transcript
+
+    if ($config.Email.Enabled) {
+
+        $logFile = Get-ChildItem $config.Logging.LogDir | Sort-Object LastWriteTime | Select-Object -Last 1
+
+        $mailArguments = @{
+
+            SmtpServer = $config.Email.SmtpServer
+
+            Port = $config.Email.SmtpPort
+
+            From = $config.Email.SenderAddress
+
+            To = $config.Email.ReceiverAddresses
+
+            UseSsl = $config.Email.SmtpSecure
+
+            Encoding = $textEncoding
+
+            Attachments = $logFile.FullName
+
+        }
+
+        Send-MailMessage @mailArguments -ErrorAction Stop
+
+    }
+
+}
