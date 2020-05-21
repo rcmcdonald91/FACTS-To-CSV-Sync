f02b8cdb9c83bbaa755f2de15c41be7a983472cd
R. Christian McDonald <rcmcdonald91@gmail.com>
2020-05-21 19:05:45 -0400

    Update FactstoCleverCSV.ps1

--- FactstoCleverCSV.ps1
+++ FactstoCleverCSV.ps1
@@ -4,115 +4,90 @@ param (
 
 )
 
-# System Settings 
-$textEncoding = [System.Text.Encoding]::UTF8 
+Import-Module -Name "$PSScriptRoot\functions.psm1" -Force
 
-$config = Get-Content -Path $ConfigFile | ConvertFrom-Json
+# READ AND PARSE CONFIG FILE
+$config = Get-Content -Path $ConfigFile -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop
+
+# BEGIN LOGGING
+if($config.General.LoggingEnabled) {
+
+    Start-Transcript -OutputDirectory $config.Paths.LogDir
+    
+}
+
+# LOG RUNNING CONFIG
+Write-Host "$($config | Format-List | Out-String)" -ForegroundColor Green
+
+# READ ODBC Arguments
+$OdbcArgs = @{}
+
+$config.OdbcArgs.PSObject.Properties | Foreach-Object { $OdbcArgs[$_.Name] = $_.Value }
 
 Write-Host "[$(Get-Date)] Script Started..." -ForegroundColor Green
 
 Write-Host "[$(Get-Date)] Host: $env:computername" -ForegroundColor Green
 
-if($config.Logging.Enabled) {
-
-    Start-Transcript -OutputDirectory $config.Logging.LogDir
-    
-}
-
-if($config.PreAction.Enabled) {
-    
-    Write-Host "[$(Get-Date)] Executing Pre Action" -ForegroundColor Green
-
-}
-
 # DELETE OLD CSV FILES
-Get-ChildItem -Path $config.General.OutputDir -Recurse -Force | Where-Object { -not ($_.PSIsContainer) } | Remove-Item -Force
+Get-ChildItem -Path $config.Paths.OutputDir -Recurse -Force | Where-Object { -not ($_.PSIsContainer) } | Remove-Item -Force
 
 # ITERATE THROUGH EACH SQL QUERY AND EXECUTE THEM
-Get-ChildItem -Path $config.General.SqlDir -Filter *.sql | ForEach-Object {
+Get-ChildItem -Path $config.Paths.SqlDir -Filter *.sql | ForEach-Object {
 
-    Write-Host "[$(Get-Date)] [SQL] [CONNECTION] $($_.FullName)" -ForegroundColor Green
-
-    # CREATE NEW ODBC CONNECTION OBJECT
-    $conn = New-Object System.Data.Odbc.OdbcConnection
-
-    # CREATE ODBC CONNNECTION STRING
-    $conn.ConnectionString = "Driver={{SQL Server}};Server={0},{1};Database={2};Uid={3};Pwd={4}" -f $config.ODBC.ServerAddress,$config.ODBC.ServerPort,$config.ODBC.DatabaseName,$config.ODBC.LoginName,$config.ODBC.LoginPassword
-  
-    Try {
-
-        # TRY TO CONNECT TO THE ODBC CONNECTION
-        $conn.open()
-
-    } Catch {
-
-        Write-Error "[$(Get-Date)] [SQL] [ERROR] ODBC CONNECTION ERROR" -ErrorAction Stop 
-
-    }
-
+    Write-Host "[$(Get-Date)] [SQL] [READ] $($_.FullName)" -ForegroundColor Green
+ 
     # READ SQL QUERY (.sql same name as script)
-    $query = Get-Content $_.FullName
+    $query = Get-Content $_.FullName | Out-String
 
-    Write-Host "[$(Get-Date)] [SQL] [QUERY] $($_.FullName)" -ForegroundColor Green
+    Write-Host "[$(Get-Date)] [SQL] [QUERY] $($_.Name)" -ForegroundColor Green
 
     # EXECUTE SQL QUERY
-    $cmd = New-object System.Data.Odbc.OdbcCommand($query, $conn)
-
-    # CREATE ODBC DATASET
-    $ds = New-Object system.Data.DataSet
-
-    # FILL ODBC DATASET WITH DATA
-    (New-Object system.Data.odbc.odbcDataAdapter($cmd)).fill($ds) | out-null
-
-    # CLOSE AND CLEANUP
-    $conn.close()
-
-    $csvFile = $config.General.OutputDir, "$($_.BaseName).csv" -join "\"
+    $results = Get-ODBCData @OdbcArgs -Query $query
 
     Write-Host "[$(Get-Date)] [CSV] [WRITE] $csvFile" -ForegroundColor Green
 
+    $csvFile = Join-Path -Path $config.Paths.OutputDir -ChildPath "$($_.BaseName).csv"
+
     # WRITE CSV TO DISK
-    $ds.Tables[0] | Export-Csv -Path $csvFile -NoTypeInformation
+    $results | Export-Csv -Path $csvFile -NoTypeInformation
 
 }
 
-if($config.PostAction.Enabled) {
+if($config.General.PostActionEnabled) {
     
-    Write-Host "[$(Get-Date)] Executing Post Action" -ForegroundColor Green
+    Write-Host "[$(Get-Date)] [CMD] [EXECUTE] Post Action $($config.PostActionArgs)" -ForegroundColor Green
+
+    $PostActionArgs = @{}
+
+    $config.PostActionArgs.PSObject.Properties | Foreach-Object { $PostActionArgs[$_.Name] = $_.Value }
+
+    Invoke-Process @PostActionArgs
 
 }
 
 Write-Host "[$(Get-Date)] Script Completed..." -ForegroundColor Green
 
-if($config.Logging.Enabled) {
+if($config.General.LoggingEnabled) {
 
     Stop-Transcript
 
-    if ($config.Email.Enabled) {
+    if ($config.General.EmailEnabled) {
 
-        $logFile = Get-ChildItem $config.Logging.LogDir | Sort-Object LastWriteTime | Select-Object -Last 1
+        $logFile = Get-ChildItem $config.Paths.LogDir | Sort-Object LastWriteTime | Select-Object -Last 1
 
-        $mailArguments = @{
+        $MailArgs = @{}
 
-            SmtpServer   = $config.Email.SmtpServer
+        $config.MailArgs.PSObject.Properties | ForEach-Object { $MailArgs[$_.Name] = $_.Value }
 
-            Port         = $config.Email.SmtpPort
+        $MailArgs = $MailArgs + @{
 
-            From         = $config.Email.SenderAddress
-
-            To           = $config.Email.ReceiverAddresses
-
-            Subject      = $config.Email.Subject
-
-            UseSsl       = $config.Email.SmtpSecure
-
-            Encoding     = $textEncoding
+            Encoding     = [System.Text.Encoding]::UTF8
 
             Attachments  = $logFile.FullName
 
         }
 
-        Send-MailMessage @mailArguments -ErrorAction Stop
+        Send-MailMessage @MailArgs -ErrorAction Stop 
 
     }
 
