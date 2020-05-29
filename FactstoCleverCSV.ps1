b04eee577b8387fb12eaf3e208b111c5812049c0
R. Christian McDonald <rcmcdonald91@gmail.com>
2020-05-29 12:24:42 -0400

    Update FactstoCleverCSV.ps1

--- FactstoCleverCSV.ps1
+++ FactstoCleverCSV.ps1
@@ -4,10 +4,27 @@ param (
 
 )
 
-Import-Module -Name "$PSScriptRoot\functions.psm1" -Force
+try {
+
+    Import-Module -Name "$PSScriptRoot\functions.psm1" -Force
+
+} catch {
+
+    Write-Error $_.Exception.Message -ErrorAction Stop
+
+}
 
 # READ AND PARSE CONFIG FILE
 $config = Get-Content -Path $ConfigFile -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop
+
+# READ ODBC Arguments
+$OdbcArgs = @{}
+
+$config.OdbcArgs.PSObject.Properties | ForEach-Object { $OdbcArgs[$_.Name] = $_.Value }
+
+$MailArgs = @{}
+
+$config.MailArgs.PSObject.Properties | ForEach-Object { $MailArgs[$_.Name] = $_.Value }
 
 # BEGIN LOGGING
 if($config.General.LoggingEnabled) {
@@ -19,17 +36,24 @@ if($config.General.LoggingEnabled) {
 # LOG RUNNING CONFIG
 Write-Host "$($config | Format-List | Out-String)" -ForegroundColor Green
 
-# READ ODBC Arguments
-$OdbcArgs = @{}
-
-$config.OdbcArgs.PSObject.Properties | Foreach-Object { $OdbcArgs[$_.Name] = $_.Value }
-
 Write-Host "[$(Get-Date)] Script Started..." -ForegroundColor Green
 
 Write-Host "[$(Get-Date)] Host: $env:computername" -ForegroundColor Green
 
 # DELETE OLD CSV FILES
 Get-ChildItem -Path $config.Paths.OutputDir -Recurse -Force | Where-Object { -not ($_.PSIsContainer) } | Remove-Item -Force
+
+if($config.General.PreActionEnabled) {
+    
+    Write-Host "[$(Get-Date)] [CMD] [EXECUTE] Pre Action $($config.PreActionArgs | Out-String)" -ForegroundColor Green
+
+    $PreActionArgs = @{}
+
+    $config.PreActionArgs.PSObject.Properties | ForEach-Object { $PreActionArgs[$_.Name] = $_.Value }
+
+    Invoke-Process @PreActionArgs
+
+}
 
 # ITERATE THROUGH EACH SQL QUERY AND EXECUTE THEM
 Get-ChildItem -Path $config.Paths.SqlDir -Filter *.sql | ForEach-Object {
@@ -44,9 +68,9 @@ Get-ChildItem -Path $config.Paths.SqlDir -Filter *.sql | ForEach-Object {
     # EXECUTE SQL QUERY
     $results = Get-ODBCData @OdbcArgs -Query $query
 
-    Write-Host "[$(Get-Date)] [CSV] [WRITE] $csvFile" -ForegroundColor Green
-
     $csvFile = Join-Path -Path $config.Paths.OutputDir -ChildPath "$($_.BaseName).csv"
+
+    Write-Host "[$(Get-Date)] [CSV] [WRITE] $csvFile" -ForegroundColor Green
 
     # WRITE CSV TO DISK
     $results | Export-Csv -Path $csvFile -NoTypeInformation
@@ -55,11 +79,11 @@ Get-ChildItem -Path $config.Paths.SqlDir -Filter *.sql | ForEach-Object {
 
 if($config.General.PostActionEnabled) {
     
-    Write-Host "[$(Get-Date)] [CMD] [EXECUTE] Post Action $($config.PostActionArgs)" -ForegroundColor Green
+    Write-Host "[$(Get-Date)] [CMD] [EXECUTE] Post Action $($config.PostActionArgs | Out-String)" -ForegroundColor Green
 
     $PostActionArgs = @{}
 
-    $config.PostActionArgs.PSObject.Properties | Foreach-Object { $PostActionArgs[$_.Name] = $_.Value }
+    $config.PostActionArgs.PSObject.Properties | ForEach-Object { $PostActionArgs[$_.Name] = $_.Value }
 
     Invoke-Process @PostActionArgs
 
@@ -73,21 +97,11 @@ if($config.General.LoggingEnabled) {
 
     if ($config.General.EmailEnabled) {
 
-        $logFile = Get-ChildItem $config.Paths.LogDir | Sort-Object LastWriteTime | Select-Object -Last 1
+        $logFile = @(Get-ChildItem $config.Paths.LogDir | Sort-Object LastWriteTime | Select-Object FullName -Last 1)
 
-        $MailArgs = @{}
+        $outputFiles = @(Get-ChildItem $config.Paths.OutputDir | Select-Object FullName)
 
-        $config.MailArgs.PSObject.Properties | ForEach-Object { $MailArgs[$_.Name] = $_.Value }
-
-        $MailArgs = $MailArgs + @{
-
-            Encoding     = [System.Text.Encoding]::UTF8
-
-            Attachments  = $logFile.FullName
-
-        }
-
-        Send-MailMessage @MailArgs -ErrorAction Stop 
+        Send-MailMessage @MailArgs -Attachments ($logFile + $outputFiles).FullName -ErrorAction Stop 
 
     }
 
